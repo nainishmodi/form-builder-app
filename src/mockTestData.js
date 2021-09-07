b1282867701b5a998af282094bd27a92d134a291
Nainish Modi <modi_nainish2000@yahoo.com>
2021-09-07 12:22:54 +0530

    Add mockdata for unit testing

---
+++ src/mockTestData.js
@@ -0,0 +1,18 @@
+export default {
+    formBuilder: [
+      {
+        formId: "form1630866082952",
+        formName: "Hello form",
+        formCreatedDate: "2021-09-05T18:21:22.952Z",
+        formQuestions: [
+          {
+            questionId: 1630866073439,
+            questionTitle: "h",
+            questionType: "checkbox",
+            questionLists: ["abc", "xyz", "nkm"],
+          },
+        ],
+        submissions: [{ formId: "form1630866082952", 1630866073439: ["abc"] }],
+      },
+    ],
+  };
